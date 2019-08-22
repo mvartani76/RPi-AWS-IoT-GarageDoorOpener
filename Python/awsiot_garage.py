290b4f8f972e97c3061be85de54ba5809cd72684
mvartani76 <mike.vartanian@gmail.com>
2019-08-22 11:21:53 -0400

    Updated to latest AWS IOT MQTT framework.

--- Python/awsiot_garage.py
+++ Python/awsiot_garage.py
@@ -1,20 +1,28 @@
-#!/usr/bin/python
+'''
+/*
+ * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+ '''
 
-# this source is part of my Hackster.io project:  https://www.hackster.io/mariocannistra/radio-astronomy-with-rtl-sdr-raspberrypi-and-amazon-aws-iot-45b617
-
-# use this program to test the AWS IoT certificates received by the author
-# to participate to the spectrogram sharing initiative on AWS cloud
-
-# this program will subscribe and show all the messages sent by its companion
-# awsiotpub.py using the AWS IoT hub
-
-import paho.mqtt.client as paho
-import os
-import socket
-import ssl
-import RPi.GPIO as GPIO
-import json
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
 import time
+import argparse
+import json
+import RPi.GPIO as GPIO
+
+AllowedActions = ['both', 'publish', 'subscribe']
 
 def setup_GPIO():
 	GPIO.setmode(GPIO.BCM)
@@ -24,17 +32,16 @@ def setup_GPIO():
 	GPIO.setup(22,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	GPIO.output(17,GPIO.HIGH)
 
-def on_connect(client, userdata, flags, rc):
-    print("Connection returned result: " + str(rc) )
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("#" , 1 )
-
-def on_message(client, userdata, msg):
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
 	global InitialGPIOSetup
-	print("topic: "+msg.topic)
-	print("payload: "+str(msg.payload))
-	if msg.topic == "Garage":
+	print("Received a new message: ")
+	print(message.payload)
+	print("from topic: ")
+	print(message.topic)
+	print("--------------\n\n")
+
+	if message.topic == "Garage":
 		# Setup the GPIO pins the first time one attempts to open/close garage
 		# This resolves a previous issue where the power cycle caused the GPIOs
 		# to go from HIGH to LOW --> end result is that garage opens/closes when
@@ -42,7 +49,7 @@ def on_message(client, userdata, msg):
 		if InitialGPIOSetup == True:
 			setup_GPIO()
 			InitialGPIOSetup = False
-		json_msg = json.loads(msg.payload.decode())
+		json_msg = json.loads(message.payload.decode())
 		if json_msg["state"]["reported"]["ON_OFF"] == "ON":
 			print "GPIO HIGH"
 			GPIO.output(int(json_msg["state"]["reported"]["GPIO"]),GPIO.HIGH)
@@ -57,31 +64,99 @@ def on_message(client, userdata, msg):
 			print "GETTING STATUS"
 			garagestatus = GPIO.input(int(json_msg["state"]["reported"]["GPIO"]))
 			if garagestatus == 1:
-				client.publish("Garage","{\"state\":{\"reported\":{\"ON_OFF\":\"UPDATE_STATUS\",\"DATA\":\"SHUT\"}}}")
+				myAWSIoTMQTTClient.publish("Garage","{\"state\":{\"reported\":{\"ON_OFF\":\"UPDATE_STATUS\",\"DATA\":\"SHUT\"}}}",0)
 			else:
-				client.publish("Garage","{\"state\":{\"reported\":{\"ON_OFF\":\"UPDATE_STATUS\",\"DATA\":\"OPEN\"}}}")
+				myAWSIoTMQTTClient.publish("Garage","{\"state\":{\"reported\":{\"ON_OFF\":\"UPDATE_STATUS\",\"DATA\":\"OPEN\"}}}",0)
 
-#def on_log(client, userdata, level, msg):
-#    print(msg.topic+" "+str(msg.payload))
+# Read in command-line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
+parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
+parser.add_argument("-p", "--port", action="store", dest="port", type=int, help="Port number override")
+parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
+                    help="Use MQTT over WebSocket")
+parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="garage",
+                    help="Targeted client id")
+parser.add_argument("-t", "--topic", action="store", dest="topic", default="Garage", help="Targeted topic")
+parser.add_argument("-m", "--mode", action="store", dest="mode", default="both",
+                    help="Operation modes: %s"%str(AllowedActions))
+parser.add_argument("-M", "--message", action="store", dest="message", default="Hello World!",
+                    help="Message to publish")
+
+args = parser.parse_args()
+host = args.host
+rootCAPath = args.rootCAPath
+certificatePath = args.certificatePath
+privateKeyPath = args.privateKeyPath
+port = args.port
+useWebsocket = args.useWebsocket
+clientId = args.clientId
+topic = args.topic
+
+if args.mode not in AllowedActions:
+    parser.error("Unknown --mode option %s. Must be one of %s" % (args.mode, str(AllowedActions)))
+    exit(2)
+
+if args.useWebsocket and args.certificatePath and args.privateKeyPath:
+    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
+    exit(2)
+
+if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
+    parser.error("Missing credentials for authentication.")
+    exit(2)
+
+# Port defaults
+if args.useWebsocket and not args.port:  # When no port override for WebSocket, default to 443
+    port = 443
+if not args.useWebsocket and not args.port:  # When no port override for non-WebSocket, default to 8883
+    port = 8883
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+if useWebsocket:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
+    myAWSIoTMQTTClient.configureEndpoint(host, port)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+else:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+    myAWSIoTMQTTClient.configureEndpoint(host, port)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+if args.mode == 'both' or args.mode == 'subscribe':
+    myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
+time.sleep(2)
 
 InitialGPIOSetup = True
-mqttc = paho.Client()
-mqttc.on_connect = on_connect
-mqttc.on_message = on_message
-#mqttc.on_log = on_log
 
-# Insert AWS host information that is given when creating Resource
-awshost = "data.iot.us-east-1.amazonaws.com"
-awsport = 8883
-clientId = "GarageDoorOpener"
-thingName = "GarageDoorOpener"
-
-caPath = "aws-iot-rootCA.crt"
-certPath = "cert.pem"
-keyPath = "privkey.pem"
-
-mqttc.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
-
-mqttc.connect(awshost, awsport, keepalive=60)
-
-mqttc.loop_forever()
+# Publish to the same topic in a loop forever
+loopCount = 0
+while True:
+    #if args.mode == 'both' or args.mode == 'publish':
+    #    message = {}
+    #    message['message'] = args.message
+    #    message['sequence'] = loopCount
+    #    messageJson = json.dumps(message)
+    #    myAWSIoTMQTTClient.publish(topic, messageJson, 1)
+    #    if args.mode == 'publish':
+    #        print('Published topic %s: %s\n' % (topic, messageJson))
+    #    loopCount += 1
+    time.sleep(1)
