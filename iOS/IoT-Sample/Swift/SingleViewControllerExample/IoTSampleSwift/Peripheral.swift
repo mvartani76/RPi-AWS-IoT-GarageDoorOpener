8799fed3acdcbf36022552c5ade2d7d11d45050b
Mike Vartanian <mike.vartanian@gmail.com>
2020-10-21 22:34:11 -0400

    Initial add of BLE code from the BLEParamController project. This code starts up the ble processing when the user presses the 'Enable Bluetooth' button. Will need to update the characteristics/services to be specific to this application.

---
+++ iOS/IoT-Sample/Swift/SingleViewControllerExample/IoTSampleSwift/Peripheral.swift
@@ -0,0 +1,23 @@
+//
+//  Peripheral.swift
+//  IoTSampleSwift
+//
+//  Created by Michael Vartanian on 10/20/20.
+//  Copyright © 2020 Amazon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreBluetooth
+
+class Peripheral: NSObject {
+
+    /// MARK: - Peripheral Parameter services and charcteristics Identifiers
+
+    public static let peripheralParamServiceUUID    = CBUUID.init(string: "6e400001-b5a3-f393-e0a9-e50e24dcca9e")
+    public static let param1CharacteristicUUID      = CBUUID.init(string: "6e400002-b5a3-f393-e0a9-e50e24dcca9e")
+    public static let param2CharacteristicUUID      = CBUUID.init(string: "6e400003-b5a3-f393-e0a9-e50e24dcca9e")
+    public static let param3CharacteristicUUID      = CBUUID.init(string: "6e400004-b5a3-f393-e0a9-e50e24dcca9e")
+    public static let paramButtonCharacteristicUUID      = CBUUID.init(string: "6e400005-b5a3-f393-e0a9-e50e24dcca9e")
+    public static let txCharacteristicUUID      = CBUUID.init(string: "6e400006-b5a3-f393-e0a9-e50e24dcca9e")
+}
--- iOS/IoT-Sample/Swift/SingleViewControllerExample/IoTSampleSwift/PublishViewController.swift
+++ iOS/IoT-Sample/Swift/SingleViewControllerExample/IoTSampleSwift/PublishViewController.swift
@@ -17,13 +17,14 @@ import UIKit
 import AWSIoT
 import AWSMobileClient
 import CoreLocation
+import CoreBluetooth
 
 enum SerializationError: Error {
     case missing(String)
     case invalid(String, Any)
 }
 
-class PublishViewController: UIViewController, CLLocationManagerDelegate {
+class PublishViewController: UIViewController, CLLocationManagerDelegate, CBPeripheralDelegate, CBCentralManagerDelegate {
 
     @IBOutlet var bluetoothEnable: UIButton!
     @IBOutlet weak var garage1TOGGLE: UIButton!
@@ -41,13 +42,161 @@ class PublishViewController: UIViewController, CLLocationManagerDelegate {
     @objc var iot: AWSIoT!
     @objc var connected = false;
 
-    var bluetoothEnabled = false
+    var bluetoothButtonEnabled = false
+    var bleReady = false
 
     var mqttStatus: String = "Disconnected"
     var topic: String = "Garage"
     
     var timer = Timer()
     
+    // Properties
+    private var centralManager: CBCentralManager!
+    private var peripheral: CBPeripheral!
+
+    // Characteristics
+    private var paramsChar: [CBCharacteristic?] = []
+
+    var charArray: [String] = [""]
+
+    // If we're powered on, start scanning
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print("Central state update")
+        if central.state != .poweredOn {
+            print("Central is not powered on")
+        } else {
+            bleReady = true
+        }
+    }
+
+    // Handles the result of the scan
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+
+        // We've found it so stop scan
+        self.centralManager.stopScan()
+
+        // Copy the peripheral instance
+        self.peripheral = peripheral
+        self.peripheral.delegate = self
+
+        // Connect!
+        self.centralManager.connect(self.peripheral, options: nil)
+    }
+
+    // The handler if we do connect succesfully
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        if peripheral == self.peripheral {
+            print("Connected to the Peripheral")
+            peripheral.discoverServices([Peripheral.peripheralParamServiceUUID])
+        }
+    }
+
+    // Handler for disconnects
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+
+        if peripheral == self.peripheral {
+            print("Disconnected")
+            // Set any GUI values to disabled here -- xxx.isEnabled = false
+
+            self.peripheral = nil
+
+            // Start scanning again
+            print("Central scanning for", Peripheral.peripheralParamServiceUUID);
+            centralManager.scanForPeripherals(withServices: [Peripheral.peripheralParamServiceUUID],
+                                              options: [CBCentralManagerScanOptionAllowDuplicatesKey : true])
+        }
+    }
+
+    // Handles discovery event
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let services = peripheral.services {
+            for service in services {
+                if service.uuid == Peripheral.peripheralParamServiceUUID {
+                    print("Param service found")
+                    //Now kick off discovery of characteristics
+                    peripheral.discoverCharacteristics([
+                        Peripheral.param1CharacteristicUUID,
+                        Peripheral.param2CharacteristicUUID,
+                        Peripheral.param3CharacteristicUUID,
+                        Peripheral.paramButtonCharacteristicUUID,
+                        Peripheral.txCharacteristicUUID], for: service)
+                    return
+                }
+            }
+        }
+    }
+
+    // Handling discovery of characteristics
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                if characteristic.uuid == Peripheral.param1CharacteristicUUID {
+                    print("Param 1 characteristic found")
+                } else if characteristic.uuid == Peripheral.param2CharacteristicUUID {
+                    print("Param 2 characteristic found")
+                } else if characteristic.uuid == Peripheral.param3CharacteristicUUID {
+                    print("Param 3 characteristic found");
+                } else if characteristic.uuid == Peripheral.paramButtonCharacteristicUUID {
+                    print("Param Button characteristic found");
+                } else if characteristic.uuid == Peripheral.txCharacteristicUUID {
+                    print("Tx characteristic found")
+                    peripheral.setNotifyValue(true, for: characteristic)
+                }
+            }
+        }
+    }
+
+    // Handle notification updates
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic,
+                    error: Error?) {
+        switch characteristic.uuid {
+            case Peripheral.param1CharacteristicUUID:
+                print("Param1 value = \(String(describing: characteristic.value))")
+            case Peripheral.param2CharacteristicUUID:
+                print("Param1 value = \(String(describing: characteristic.value))")
+            case Peripheral.param3CharacteristicUUID:
+                print("Param3 value = \(String(describing: characteristic.value))")
+            case Peripheral.paramButtonCharacteristicUUID:
+                print("ParamButton value = \(String(describing: characteristic.value))")
+            case Peripheral.txCharacteristicUUID:
+                // values are coming over as bytes from the peripheral so need to convert to whatever expected data type
+                if let charStringTmp = String(bytes: characteristic.value!, encoding: .utf8) {
+                    charArray.append(charStringTmp)
+                    if charArray.count >= 5 {
+                        charArray.remove(at: 0)
+                    }
+                    let stringPrint = charArray.joined(separator: "")
+
+                    //rxTextLabel.text = stringPrint
+                } else {
+                    print("not a valid UTF-8 sequence")
+                }
+            default:
+                print("Unhandled Characteristic UUID: \(characteristic.uuid)")
+        }
+    }
+
+    // Handle peripheral modify services
+    func peripheral(_ peripheral: CBPeripheral,
+                    didModifyServices invalidatedServices: [CBService]) {
+        print("Services Invalidated...")
+        for service in invalidatedServices {
+            if let characteristics = service.characteristics {
+                for characteristic in characteristics {
+                        print(characteristic)
+                    }
+            }
+        }
+    }
+
+    private func writeValueToChar( withCharacteristic characteristic: CBCharacteristic, withValue value: Data) {
+        // Check if it has the write property
+        // Still need to investigate how to send without response
+        if characteristic.properties.contains(.write) && peripheral != nil {
+            peripheral.writeValue(value, for: characteristic, type: .withResponse)
+        }
+    }
+
     func mqttEventCallback( _ status: AWSIoTMQTTStatus )
     {
         DispatchQueue.main.async {
@@ -219,6 +368,8 @@ class PublishViewController: UIViewController, CLLocationManagerDelegate {
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         self.locationManager.requestWhenInUseAuthorization()
+
+        centralManager = CBCentralManager(delegate: self, queue: nil)
         
         // Initialize AWSMobileClient for authorization
         AWSMobileClient.default().initialize { (userState, error) in
@@ -295,14 +446,24 @@ class PublishViewController: UIViewController, CLLocationManagerDelegate {
     @IBAction func wasEnableBluetoothButtonPressed(_ sender: UIButton) {
         sender.shrink()
 
-        if (bluetoothEnabled == false) {
-            bluetoothEnabled = true
-
-            sender.setTitle("Bluetooth Enabled", for: .normal)
-            sender.setTitle("Bluetooth Enabled", for: .highlighted)
+        if (bluetoothButtonEnabled == false) {
+            if (bleReady) {
+                bluetoothButtonEnabled = true
+                sender.setTitle("Bluetooth Enabled", for: .normal)
+                print("Central scanning for", Peripheral.peripheralParamServiceUUID);
+                centralManager.scanForPeripherals(withServices: [Peripheral.peripheralParamServiceUUID],
+                                                  options: [CBCentralManagerScanOptionAllowDuplicatesKey : true])
+            } else {
+                sender.setTitle("Try Again", for: .normal)
+                sender.layer.borderColor = UIColor.red.cgColor
+                sender.layer.borderWidth = 5
+            }
         } else {
-            bluetoothEnabled = false
+            bluetoothButtonEnabled = false
+            // Stop scanning
+            self.centralManager.stopScan()
             bluetoothEnable.setTitle("Enable Bluetooth", for: .normal)
+            sender.layer.borderWidth = 0
         }
 
     }
