faa6e68503d702aabca0090878fc14f984aa6f02
mvartani76 <mike.vartanian@gmail.com>
2020-11-26 23:48:59 -0500

    Updated spacing to be consistent throughout. Tabs and spaces produces errors. This addresses #9.

--- Python/ble-uart-peripheral/uart_peripheral.py
+++ Python/ble-uart-peripheral/uart_peripheral.py
@@ -56,7 +56,7 @@ class TxCharacteristic(Characteristic):
             return
         value = []
         for c in s:
-	    print(c)
+            print(c)
             value.append(dbus.Byte(c.encode()))
         self.PropertiesChanged(GATT_CHRC_IFACE, {'Value': value}, [])
 
@@ -74,52 +74,52 @@ class RxCharacteristic(Characteristic):
     def __init__(self, bus, index, param_uuid, service):
         Characteristic.__init__(self, bus, index, param_uuid,
                                 ['read','write','write-without-response','notify','authorize'], service)
-	self.notifying = False
+        self.notifying = False
 
     def send_tx(self, s):
         if not self.notifying:
             return
         value = []
         for c in s:
-	    print(c)
+            print(c)
             value.append(dbus.Byte(c.encode()))
         self.PropertiesChanged(GATT_CHRC_IFACE, {'Value': value}, [])
 
     def WriteValue(self, value, options):
 	# convert value into UInt8
-	temp = [hex(c) for c in value]
-	int8Value = int(temp[0],0)
-	print("uuid: %s value: %s " % (self.uuid, int8Value))
+        temp = [hex(c) for c in value]
+        int8Value = int(temp[0],0)
+        print("uuid: %s value: %s " % (self.uuid, int8Value))
 
-	if int8Value == GARAGE1TOGGLE_BTVALUE:
-		print("Garage 1 toggle")
-		GPIO.output(17, GPIO.LOW)
-		time.sleep(0.2)
-		GPIO.output(17, GPIO.HIGH)
-		time.sleep(3)
-	elif int8Value == GARAGE2TOGGLE_BTVALUE:
-		print("Garage 2 toggle")
-		GPIO.output(27, GPIO.LOW)
-		time.sleep(0.2)
-		GPIO.output(27, GPIO.HIGH)
-		time.sleep(3)
-	elif int8Value == REQUESTGARAGE1STATUS_BTVALUE:
-		print("garage 1 status")
-		garagestatus = GPIO.input(17)
-		if garagestatus == 1:
-			self.send_tx("Garage 1 is Closed")
-		else:
-			self.send_tx("Garage 1 is Open")
-	elif int8Value == REQUESTGARAGE2STATUS_BTVALUE:
-		print("garage 2 status")
-		garagestatus = GPIO.input(27)
-		if garagestatus == 1:
-			self.send_tx("Garage 2 is Open")
-		else:
-			self.send_tx("Garage 2 is Closed")
-		TxCharacteristic.send_tx(garagestatus)
-	else:
-		print("Invalid Command.")
+        if int8Value == GARAGE1TOGGLE_BTVALUE:
+            print("Garage 1 toggle")
+            GPIO.output(17, GPIO.LOW)
+            time.sleep(0.2)
+            GPIO.output(17, GPIO.HIGH)
+            time.sleep(3)
+        elif int8Value == GARAGE2TOGGLE_BTVALUE:
+            print("Garage 2 toggle")
+            GPIO.output(27, GPIO.LOW)
+            time.sleep(0.2)
+            GPIO.output(27, GPIO.HIGH)
+            time.sleep(3)
+        elif int8Value == REQUESTGARAGE1STATUS_BTVALUE:
+            print("garage 1 status")
+            garagestatus = GPIO.input(17)
+            if garagestatus == 1:
+                self.send_tx("Garage 1 is Closed")
+            else:
+                self.send_tx("Garage 1 is Open")
+        elif int8Value == REQUESTGARAGE2STATUS_BTVALUE:
+            print("garage 2 status")
+            garagestatus = GPIO.input(27)
+            if garagestatus == 1:
+                self.send_tx("Garage 2 is Open")
+            else:
+                self.send_tx("Garage 2 is Closed")
+            TxCharacteristic.send_tx(garagestatus)
+        else:
+            print("Invalid Command.")
 
     def StartNotify(self):
         if self.notifying:
@@ -134,11 +134,11 @@ class RxCharacteristic(Characteristic):
 class UartService(Service):
     def __init__(self, bus, index):
         Service.__init__(self, bus, index, UART_SERVICE_UUID, True)
-	self.add_characteristic(RxCharacteristic(bus, 1, UART_SLIDER1_RX_CHARACTERISTIC_UUID, self))
-	self.add_characteristic(RxCharacteristic(bus, 2, UART_SLIDER2_RX_CHARACTERISTIC_UUID, self))
-	self.add_characteristic(RxCharacteristic(bus, 3, UART_SLIDER3_RX_CHARACTERISTIC_UUID, self))
-	self.add_characteristic(RxCharacteristic(bus, 4, UART_BUTTON_RX_CHARACTERISTIC_UUID, self))
-	self.add_characteristic(TxCharacteristic(bus, 5, self))
+        self.add_characteristic(RxCharacteristic(bus, 1, UART_SLIDER1_RX_CHARACTERISTIC_UUID, self))
+        self.add_characteristic(RxCharacteristic(bus, 2, UART_SLIDER2_RX_CHARACTERISTIC_UUID, self))
+        self.add_characteristic(RxCharacteristic(bus, 3, UART_SLIDER3_RX_CHARACTERISTIC_UUID, self))
+        self.add_characteristic(RxCharacteristic(bus, 4, UART_BUTTON_RX_CHARACTERISTIC_UUID, self))
+        self.add_characteristic(TxCharacteristic(bus, 5, self))
 
 class Application(dbus.service.Object):
     def __init__(self, bus):
